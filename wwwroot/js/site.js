edce5fdb63e5f3cd724b0127d21cb7c5c4004c71
Mehmet Emin Gocen <mehmetemingocen188@gmail.com>
2025-07-20 18:29:05 +0300

    header search bar'a sesli arama özelliği eklendi

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function startListening() {
+        if (!('webkitSpeechRecognition' in window)) {
+            alert("Tarayıcınız sesli yazmayı desteklemiyor.");
+            return;
+        }
+
+        const recognition = new webkitSpeechRecognition();
+        recognition.lang = "tr-TR"; // Türkçe için
+        recognition.continuous = false;
+        recognition.interimResults = false;
+
+        recognition.onstart = function () {
+            console.log("Dinleniyor...");
+        };
+
+        recognition.onresult = function (event) {
+            const transcript = event.results[0][0].transcript;
+            document.getElementById("searchInput").value = transcript;
+        };
+
+        recognition.onerror = function (event) {
+            console.error("Hata:", event.error);
+        };
+
+        recognition.start();
+    }
